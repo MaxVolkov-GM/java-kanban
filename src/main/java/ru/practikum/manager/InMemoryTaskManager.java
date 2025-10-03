d8b5ebd73df00e2e616745989e020f539b8a8e74
Maxim Grinev <mrdarkvolkov@gmail.com>
2025-10-03 19:55:38 +0300

    fix

--- src/main/java/ru/practikum/manager/InMemoryTaskManager.java
+++ src/main/java/ru/practikum/manager/InMemoryTaskManager.java
@@ -184,6 +184,41 @@ public class InMemoryTaskManager implements TaskManager {
         return historyManager.getHistory();
     }
 
+    @Override
+    public void deleteTasks() {
+        for (Integer taskId : tasks.keySet()) {
+            historyManager.remove(taskId);
+        }
+        tasks.clear();
+    }
+
+    @Override
+    public void deleteSubtasks() {
+        for (Integer subtaskId : subtasks.keySet()) {
+            historyManager.remove(subtaskId);
+        }
+        subtasks.clear();
+        
+        // Очищаем подзадачи у всех эпиков
+        for (Epic epic : epics.values()) {
+            epic.getSubtaskIds().clear();
+            updateEpicStatus(epic);
+        }
+    }
+
+    @Override
+    public void deleteEpics() {
+        // Удаляем все подзадачи эпиков из истории и хранилища
+        for (Epic epic : epics.values()) {
+            for (Integer subtaskId : epic.getSubtaskIds()) {
+                historyManager.remove(subtaskId);
+                subtasks.remove(subtaskId);
+            }
+            historyManager.remove(epic.getId());
+        }
+        epics.clear();
+    }
+
     private void updateEpicStatus(Epic epic) {
         List<Integer> subtaskIds = epic.getSubtaskIds();
         if (subtaskIds.isEmpty()) {
